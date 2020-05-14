feb6cea7cf1cc0c01fb596970d61ade2b8755cd9
as9934 <40156402+as9934@users.noreply.github.com>
2020-05-14 17:49:31 -0400

    Add files via upload

---
+++ linkedin_scrape.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome('/Users/as9934/Downloads/chromedriver')
+
+
---
+++ parameters.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+search_query = 'site:linkedin.com/in/ AND "Berkeley" AND "UNC" AND "Journalism"'
+
+
+# In[2]:
+
+
+file_name = 'results_file.csv'
+
+
+# In[3]:
+
+
+linkedin_username = 'arijit.douglas.sen@gmail.com'
+
+
+# In[4]:
+
+
+linkedin_password = 'pspgoman9934'
+
+
+# In[ ]:
+
+
+
+
